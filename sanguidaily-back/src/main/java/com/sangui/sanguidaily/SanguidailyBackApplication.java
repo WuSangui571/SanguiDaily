f3e94459b9f633f5cafff946d352cbcac58cf800
sangui <shouhaoze@stu.sjzu.edu.cn>
2026-01-09 20:47:13 +0800

    first commit

---
+++ sanguidaily-back/src/main/java/com/sangui/sanguidaily/SanguidailyBackApplication.java
@@ -0,0 +1,13 @@
+package com.sangui.sanguidaily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanguidailyBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SanguidailyBackApplication.class, args);
+    }
+
+}
